5f173b089816e02a95b4225167c85b748a83361d
Peter Hartree <web@peterhartree.co.uk>
2015-12-12 16:00:43 +0000

    yo angular
    Signed-off-by: Peter Hartree <web@peterhartree.co.uk>

---
+++ app/scripts/app.js
@@ -0,0 +1,36 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name wpJsBpApp
+ * @description
+ * # wpJsBpApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('wpJsBpApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch',
+    'wp.api'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name wpJsBpApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the wpJsBpApp
+ */
+angular.module('wpJsBpApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
